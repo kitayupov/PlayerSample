5a0b3d63fe7f6c1798955cc222999f60b7f2a505
kam.tayupov <k.i.tayupov@mail.ru>
2018-02-06 16:43:18 +0300

    implement play/pause

--- mediacontroller/src/main/java/com/kbnt/qam/mediacontroller/MediaController.java
+++ mediacontroller/src/main/java/com/kbnt/qam/mediacontroller/MediaController.java
@@ -3,11 +3,19 @@ package com.kbnt.qam.mediacontroller;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MediaController extends View {
 
+    private static final String TAG = MediaController.class.getSimpleName();
+
     private ControlListener controlListener;
+
+    private enum Status {PLAY, PAUSE}
+
+    private Status status;
 
     public MediaController(Context context) {
         this(context, null);
@@ -23,11 +31,52 @@ public class MediaController extends View {
 
     public MediaController(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        initialize();
+    }
+
+    private void initialize() {
+        status = Status.PLAY;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        onTouchListener.onTouch(this, event);
+        return super.onTouchEvent(event);
     }
 
     public void setControlListener(ControlListener controlListener) {
         this.controlListener = controlListener;
     }
+
+    private OnTouchListener onTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Log.e(TAG, "onTouch: " + event.getAction());
+            switch (status) {
+                case PLAY:
+                    pause();
+                    break;
+                case PAUSE:
+                    play();
+                    break;
+            }
+            return false;
+        }
+
+        private void play() {
+            if (controlListener != null) {
+                controlListener.play();
+                status = Status.PLAY;
+            }
+        }
+
+        private void pause() {
+            if (controlListener != null) {
+                controlListener.pause();
+                status = Status.PAUSE;
+            }
+        }
+    };
 
     public interface ControlListener {
         void play();
